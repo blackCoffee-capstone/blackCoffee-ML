83d89957cda3e0c73d14fdf3eb0388779461bce4
iknow <oss002@ajou.ac.kr>
2022-09-29 07:00:28 +0000

    nothing

---
+++ postClassifier/classifyPost.py
@@ -0,0 +1,18 @@
+
+import pandas as pd
+import model, utils
+
+"""
+specify input csv file path, and output path
+"""
+
+
+input_path  = '/input/posts.csv'
+output_path = '/output/posts_classified.csv'
+
+
+
+
+
+
+
