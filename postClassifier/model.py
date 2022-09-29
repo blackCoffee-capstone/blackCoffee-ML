13ee398df63f47f27a6e3801c057d798ae230ab2
iknow <oss002@ajou.ac.kr>
2022-09-29 06:08:15 +0000

    fresh commit

---
+++ postClassifier/model.py
@@ -0,0 +1,24 @@
+import torch
+import pandas as pd
+import transformer
+
+
+
+class BCTokenizer():
+    def __init__():
+        self.mecab
+
+
+class BCClassifier():
+
+    
+    def __init__():
+        None
+    
+    def forward():
+        None
+        
+        
+    
+    
+
