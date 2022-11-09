470b4f8a1ff59ec494bc77389dd15340b4820a3b
Donghoon Lee <oss002@ajou.ac.kr>
2022-11-09 13:31:01 +0900

    Blueprint laid for simple preprocessor

---
+++ postClassifier/preprocessor.py
@@ -0,0 +1,81 @@
+import pandas as pd
+
+
+class PreProcessor():
+    def __init__(
+        self
+    ):
+        return
+
+    def __call__(
+        self,
+        input_string
+    ):
+        return self.preProcess(input_string)
+
+    def preProcess(
+        self,
+        input_string
+    ):
+        if self._isNonKorean(input_string):
+            if self._isNonEnglish(input_string):
+                return ''
+            
+        output_string = self._removeEmoji(input_string)
+        output_string = self._addSpaceAfterPunctuation(output_string)
+        output_string = self._addSpaceBeforeHashtag(output_string)
+        output_string = self._removeDoubleSpace(output_string)
+
+        return output_string
+    
+    def _isNonKorean(
+        self,
+        input_string
+    ):  
+        
+        if():
+            return True
+
+        else:
+            return False
+
+    def _isNonEnglish(
+        self,
+        input_string
+    ):  
+        
+        if():
+            return True
+
+        else:
+            return False
+
+    def _removeEmoji(
+        self,
+        input_string
+    ):
+        output_string = input_string
+        return output_string
+
+    def _addSpaceAfterPunctuation(
+        self,
+        input_string
+    ):
+        output_string = input_string
+        return output_string
+
+    def _addSpaceBeforeHashtag(
+        self,
+        input_string
+    ):
+        output_string = input_string
+        return output_string
+
+
+    def _removeDoubleSpace(
+        self,
+        input_string
+    ):
+        output_string = input_string
+        return output_string
+
