14f740bde14d9e3660bea438ffb2721a207d90fe
Donghoon Lee <oss002@ajou.ac.kr>
2022-11-10 16:04:59 +0900

    emergency commit

--- postClassifier/classifyPost.py
+++ postClassifier/classifyPost.py
@@ -1,6 +1,6 @@
-
 import pandas as pd
 import model, utils
+from utils import DataLoader
 
 """
 specify input csv file path, and output path
@@ -11,8 +11,20 @@ input_path  = '/input/posts.csv'
 output_path = '/output/posts_classified.csv'
 
 
+"""
+First determine if the post is travel related or not
+"""
 
 
 
+output = None
+
+"""
+Extract tag information from post
+"""
 
 
+
+"""
+Export to file
+"""
---
+++ postClassifier/utils.py
@@ -0,0 +1,29 @@
+from preprocessor import PreProcessor
+
+class DataLoader():
+    def __init__(
+        self,
+        num_workers,
+        batch_size
+    ):
+        self.num_workers = num_workers
+        self.batch_size = batch_size
+        self.pre_processor = PreProcessor()
+
+    def load_insta_post():
+        None
+
+class DataExporter():
+    """
+        Export Datas 
+    """
+    
+    def __init__(
+        self,
+        num_workers
+    ):
+        self.num_workers = num_workers
+
+        
+
+    
