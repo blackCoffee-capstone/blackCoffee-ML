4f2ac5db3511813f1e82edce47d3ff6e244a6968
Donghoon Lee <oss002@ajou.ac.kr>
2022-11-11 17:41:42 +0900

    Language Filtering Tested and Works

--- postClassifier/preprocessor.py
+++ postClassifier/preprocessor.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import langid
+import re
 
 class PreProcessor():
     def __init__(
@@ -18,11 +19,10 @@ class PreProcessor():
     def preProcess(
         self,
         input_string
-    ):
-        if self._isNonKorean(input_string):
-            if self._isNonEnglish(input_string):
-                return ''
-            
+    ):  
+        if self._isNonKorean(input_string) and self._isNonEnglish(input_string):
+            return ''
+        
         output_string = self._removeEmoji(input_string)
         output_string = self._addSpaceAfterPunctuation(output_string)
         output_string = self._addSpaceBeforeHashtag(output_string)
@@ -35,47 +35,62 @@ class PreProcessor():
         self,
         input_string
     ):  
-        
-        if():
+        try: 
+            detected_languages = langid.rank(input_string)
+        except:
             return True
-
-        else:
+        
+        if ('ko' == detected_languages[0][0]):
             return False
-
+        else :
+            return True
+        
+        
 
     def _isNonEnglish(
         self,
         input_string
     ):  
-        
-        if():
+        try: 
+            detected_languages = langid.rank(input_string)
+        except:
             return True
 
-        else:
+        if ('en' == detected_languages[0][0]):
             return False
+        else :
+            return True
 
 
     def _removeEmoji(
         self,
         input_string
     ):
-        output_string = input_string
+        
+        regrex_pattern = re.compile(pattern = "["
+            u"\U0001F600-\U0001F64F"  # emoticons
+            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+            u"\U0001F680-\U0001F6FF"  # transport & map symbols
+            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        "]+", flags = re.UNICODE)
+        
+        output_string = regrex_pattern.sub(r'',input_string)
+
         return output_string
 
 
     def _addSpaceAfterPunctuation(
         self,
         input_string
-    ):
-        output_string = input_string
+    ):  
+        output_string =re.sub(r'([.,>)}!?])',r'\1 ',input_string)
         return output_string
-
 
     def _addSpaceBeforeHashtag(
         self,
         input_string
     ):
-        output_string = input_string
+        output_string =re.sub(r'(#+)',r' \1',input_string)
         return output_string
 
 
@@ -83,7 +98,7 @@ class PreProcessor():
         self,
         input_string
     ):
-        output_string = input_string
+        output_string = re.sub("\s\s+" , " ", input_string)
         return output_string
 
 
@@ -98,6 +113,7 @@ def testPreProcessor():
     for pair in testData:
         if not preProcessor(pair['input']) == pair['output'] :
             errors.append('Failed')
+            print('------------\nExpected:'+pair['output']+'\nBut got:'+preProcessor(pair['input'])+'\n------------')
         else:
             errors.append(None)
 
