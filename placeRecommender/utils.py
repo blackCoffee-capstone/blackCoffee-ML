d10f02e9bbb82659ef10b82a243eebab8ab3c431
oss0430 <oss002@ajou.ac.kr>
2022-11-30 16:28:49 +0000

    Added Hybrid Recommender Dataset

--- placeRecommender/utils.py
+++ placeRecommender/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import json
 import torch
+import pickle
 from torch.utils.data import Dataset
 
 class Item():
@@ -383,69 +384,87 @@ class newRecommendationDataset():
         return new_list
 
 
-class ItemMap():
+class SpotMap():
     def __init__(
         self,
-        item_map,
-        number_of_items,
-        feature_size,
-        item_feature
+        spot_map = None,
+        number_of_spots = None,
     ):
-        self.item_map = item_map
-        self.number_of_items = number_of_items
-        self.feature_size = feature_size
-        self.item_feature = item_feature
+        self.spot_map = spot_map
+        self.number_of_spots = number_of_spots
 
-    def get_item_feature(self) :
-        return self.item_feature
-
-    def search_item(
+    def from_dfspots_make_map(
         self,
-        raw_item_id
+        df_spots
     ):
-        if raw_item_id in self.item_map :
-            return self.item_map[raw_item_id]
+        spot_record = df_spots.to_dict("records")
+        spot_map = {}
+        new_spot_id = 1
+        for spot in spot_record:
+            spot_map[spot['id']] = new_spot_id
+            new_spot_id = new_spot_id + 1
 
-        else :
-            return None
+        self.spot_map = spot_map
+        self.number_of_spots = len(spot_map)
+
+    def load_from_pickle(
+        self,
+        path
+    ) :
+        spot_map = {}
+        with open(path, 'rb') as f:
+            user_map = pickle.load(f)
+        self.spot_map = spot_map
+        self.number_of_spots = len(spot_map)
+
+    def export_to_pickle(
+        self,
+        path
+    ) :
+        spot_map = self.spot_map
+        with open(path, 'wb') as f:
+            pickle.dump(spot_map, f)
     
 class UserMap():
     def __init__(
         self,
-        user_map,
-        number_of_users,
-        feature_size,
-        user_feature
+        user_map = None,
+        number_of_users = None,
     ):
         self.user_map = user_map
         self.number_of_users = number_of_users
-        self.feature_size = feature_size
-        self.user_feature = user_feature
 
-
-    def get_item_feature(self) :
-        return self.user_feature
-
-
-    def search_item(
+    def from_dfuserTaste_make_map(
         self,
-        raw_user_id
+        df_userTaste
     ):
-        if raw_user_id in self.user_map :
-            return self.user_map[raw_user_id]
+        taste_record = df_userTaste.to_dict("records")
+        user_map = {}
+        new_user_id = 1
+        for user_taste in taste_record:
+            user_map[user_taste['id']] = new_user_id
+            new_user_id = new_user_id + 1
 
-        else :
-            return None
+        self.user_map = user_map
+        self.number_of_users = len(user_map)
 
+    def load_from_pickle(
+        self,
+        path
+    ) :
+        user_map = {}
+        with open(path, 'rb') as f:
+            user_map = pickle.load(f)
+        self.user_map = user_map
+        self.number_of_users = len(user_map)
 
-    def load_from_csv(self) :
-        self.user_map
-        self.user_feature
-
-
-    def export_as_csv(self) :
-        self.user_map
-        self.user_feature
+    def export_to_pickle(
+        self,
+        path
+    ) :
+        user_map = self.user_map
+        with open(path, 'wb') as f:
+            pickle.dump(user_map, f)
 
 
 class HybridRecDataset(Dataset):
@@ -455,19 +474,138 @@ class HybridRecDataset(Dataset):
         df_userTaste,
         df_spotFeature,
         df_userlikedspot,
-        df_uservisitedspot
+        df_uservisitedspot,
+        dict_user_feature_map = {
+            '산'           : 0,
+            '럭셔리'       : 1,
+            '역사'         : 2,
+            '웰빙'         : 3,
+            '바다'         : 4,
+            '카페'         : 5,
+            '공원'         : 6,
+            '전시장'       : 7,
+            '건축'         : 8,
+            '사찰'         : 9,
+            '가족'         : 10,
+            '학교'         : 11,
+            '놀이공원'     : 12,
+            '겨울'         : 13,
+            '엑티비티'     : 14,
+            '캠핑'         : 15,
+            '섬'           : 16,
+            '커플'         : 17,
+            '저수지'       : 18,
+            '폭포'         : 19,
+            'unknown_theme': 20
+        },
+        dict_item_feature_map = {
+            '산'           : 0,
+            '럭셔리'       : 1,
+            '역사'         : 2,
+            '웰빙'         : 3,
+            '바다'         : 4,
+            '카페'         : 5,
+            '공원'         : 6,
+            '전시장'       : 7,
+            '건축'         : 8,
+            '사찰'         : 9,
+            '가족'         : 10,
+            '학교'         : 11,
+            '놀이공원'     : 12,
+            '겨울'         : 13,
+            '엑티비티'     : 14,
+            '캠핑'         : 15,
+            '섬'           : 16,
+            '커플'         : 17,
+            '저수지'       : 18,
+            '폭포'         : 19,
+            'unknown_theme': 20,
+            '서울'         : 21,
+            '부산'         : 22,
+            '대구'         : 23,
+            '인천'         : 24,
+            '광주'         : 25,
+            '대전'         : 26,
+            '울산'         : 27,
+            '세종'         : 28,
+            '경기도'       : 29,
+            '강원도'       : 30,
+            '충청도'       : 31,
+            '전라도'       : 32,
+            '경상도'       : 33,
+            '제주'         : 34,
+            'unknown_loc'  : 35
+        }
     ):
-        self.data = None
+        df_userlikedspot = df_userlikedspot.rename(columns={"spot_id":"liked_item_id"})
+        df_visitcount = df_uservisitedspot.groupby(["user_id", "spot_id"]).size().reset_index(name="visit_count")
+        df_visitcount= df_visitcount.rename(columns={"spot_id":"visited_item_id"})
+        
+        #print(df_userlikedspot, df_visitcount)
+        df_user_item_interaction = pd.merge(left = df_userlikedspot , right = df_visitcount, left_on=["user_id","liked_item_id"], right_on=["user_id","visited_item_id"], how = "outer", sort=True)
+        def calculate_ratings(row):
+            ratings = 0.0
+
+            #print(row["liked_item_id"], row["visited_item_id"])
+            if not np.isnan(row["liked_item_id"]):
+                ratings = ratings + 3.6
+            if not np.isnan(row["visited_item_id"]):
+                ratings = ratings + 0.1 * min(row['visit_count'] ,14)
+
+            return ratings
+
+        def get_item_id(row) :
+            item_id = 0
+            if not np.isnan(row["liked_item_id"]):
+                item_id = int(row["liked_item_id"])
+            if not np.isnan(row["visited_item_id"]):
+                item_id = int(row["visited_item_id"])
+
+            return item_id
+
+        df_user_item_interaction["rating"]  = df_user_item_interaction.apply(lambda x : calculate_ratings(x), axis = 1)
+        df_user_item_interaction["item_id"] = df_user_item_interaction.apply(lambda x : get_item_id(x), axis = 1)
+        df_user_item_interaction = df_user_item_interaction[["user_id","item_id","rating"]]
+
+        def get_user_feature_ids(row):
+            user_id = row['id']
+            user_feature_ids = [0] * len(dict_user_feature_map)
+            for theme_name in row['themes']:
+                user_feature_ids[dict_user_feature_map[theme_name]] = 1
+
+            return pd.Series([user_id, user_feature_ids])
+
+        df_userTaste[["user_id","user_feature"]] = df_userTaste.apply(lambda x : get_user_feature_ids(x),axis = 1)
+        df_user_feature_ids = df_userTaste[["user_id","user_feature"]]
+
+        def get_item_feature_ids(row):
+            item_id = row['id']
+            item_feature_ids = [0] * len(dict_item_feature_map)
+            for theme_name in row['themes']:
+                item_feature_ids[dict_item_feature_map[theme_name]] = 1
+            metroName = row['metroName']
+            item_feature_ids[dict_item_feature_map[metroName]] = 1
+
+            return pd.Series([item_id, item_feature_ids])
+
+        df_spotFeature[["item_id","item_feature"]] = df_spotFeature.apply(lambda x : get_item_feature_ids(x),axis = 1)
+        df_spot_feature_ids = df_spotFeature[["item_id","item_feature"]]
+
+        df_data = pd.merge(left=df_user_item_interaction, right=df_user_feature_ids, how="left", on="user_id", sort=True)
+        df_data = pd.merge(left=df_data, right=df_spot_feature_ids, how="left", on="item_id", sort=True)
+        print(df_data)
+
+        self.data = df_data.to_dict("index")
         """
         data = {
-            "1" : {
+            "0" : {
                 "user_id" : 23,
                 "user_feature" : [0,0,1,0,1,0,1,1,1],
                 "item_id" : 543,
                 "item_feature" : [0,0,1,0,1,1,1,0,1],
                 "rating"  : 0.7
             },
-            "2" : ...
+            "1" : ...
         }
         """
 
@@ -480,11 +618,11 @@ class HybridRecDataset(Dataset):
     ):
         single_data = self.data[index]
 
-        user_id      = torch.tensor([single_data["user_id"]])
+        user_id      = torch.tensor(single_data["user_id"])
         user_feature = torch.tensor(single_data["user_feature"])
-        item_id      = torch.tensor([single_data["item_id"]])
+        item_id      = torch.tensor(single_data["item_id"])
         item_feature = torch.tensor(single_data["item_feature"])
-        rating       = torch.tensor([single_data["rating"]])
+        rating       = torch.tensor(single_data["rating"])
         
         return {
             'user_id'      : user_id.to(dtype = torch.long),
